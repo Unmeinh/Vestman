320f9650a719b136e5dd06dfd62d4c6ff0a18955
tran van trong <trongtvph21826@fpt.edu.vn>
2023-09-18 18:51:29 +0700

    screeen_signin

---
+++ app/src/main/java/com/ttonline/vestman/screen/Screen_signup.java
@@ -0,0 +1,16 @@
+package com.ttonline.vestman.screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ttonline.vestman.R;
+
+public class Screen_signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_signup);
+    }
+}
